ba0960aa45b1dedd2bc81d61637cf9053301d672
shiravneri <shiravneri@gmail.com>
2020-12-22 14:16:50 +0200

    Added gitignore

--- Heuristics.py
+++ Heuristics.py
@@ -7,5 +7,17 @@ def euclidean_distance(node_coordinates, end_point):
     return sqrt(x_delta ** 2 + y_delta ** 2)
 
 
+def manhattan_distance(node_coordinates, end_point):
+    horizontal = abs(end_point.x - node_coordinates.x)
+    vertical = abs(end_point.y - node_coordinates.y)
+    return horizontal + vertical
+
+
+def max_heuristic(node_coordinates, end_point):
+    euclidean = euclidean_distance(node_coordinates, end_point)
+    manhattan = manhattan_distance(node_coordinates, end_point)
+    return max(euclidean, manhattan)
+
+
 def zero_heuristic(node_coordinates, end_point):
     return 0
