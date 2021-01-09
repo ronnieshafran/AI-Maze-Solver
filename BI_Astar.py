c8d87d63cbf864b27122ff2367b883192200030f
shiravneri <shiravneri@gmail.com>
2021-01-09 15:24:55 +0200

    Medium test path is good, Large test path still isn't

--- BI_Astar.py
+++ BI_Astar.py
@@ -27,14 +27,6 @@ def run(data: DataInput, h_function) -> AlgorithmResult:
         current_forward_node = forward_open_queue.remove()
         current_backward_node = backward_open_queue.remove()
 
-        if current_forward_node.coordinates in forward_visited and current_forward_node.f_cost_of_path >= \
-                forward_visited.get(current_forward_node.coordinates).f_cost_of_path:
-            continue
-
-        if current_backward_node.coordinates in backward_visited and current_backward_node.f_cost_of_path >= \
-                backward_visited.get(current_backward_node.coordinates).f_cost_of_path:
-            continue
-
         # statistics calculation
         if current_forward_node.depth > max_depth:
             max_depth = current_forward_node.depth
@@ -92,8 +84,11 @@ def run(data: DataInput, h_function) -> AlgorithmResult:
         if min_val < goal_forward.g_cost_of_path + goal_backward.g_cost_of_path:
             goal_forward = optimal_path[0]
             goal_backward = optimal_path[1]
-
-        return AlgorithmResult(goal_forward.path_to_node[:-1] + fix_backward_path(goal_backward),
+        if goal_forward.path_to_node == "":
+            all_backward = True
+        else:
+            all_backward = False
+        return AlgorithmResult(goal_forward.path_to_node[:-1] + fix_backward_path(goal_backward, all_backward),
                                min_val, total_expanded_nodes, penetration, True,
                                EBF, avg_h, min_depth, max_depth, avg_depth, time.process_time() - start_time)
     else:
@@ -139,26 +134,52 @@ def find_optimal_path(forward: Node(), backward: Node(), visited_forward: {}, vi
     return optimal_path, min_val
 
 
-def fix_backward_path(node: Node()):
+def fix_backward_path(node: Node(), all_backward):
     path = node.list_of_cords[::-1]
     path.insert(0, node.coordinates)
     fixed_path = ''
     for index in range(len(path) - 1):
         if path[index].x + 1 == path[index + 1].x and path[index].y + 1 == path[index + 1].y:
-            fixed_path += "-RD"
+            if not all_backward:
+                fixed_path += "-RD"
+            else:
+                fixed_path += "RD"
         elif path[index].x + 1 == path[index + 1].x and path[index].y == path[index + 1].y:
-            fixed_path += "-D"
+            if not all_backward:
+                fixed_path += "-D"
+            else:
+                fixed_path += "D"
         elif path[index].x + 1 == path[index + 1].x and path[index].y - 1 == path[index + 1].y:
-            fixed_path += "-LD"
+            if not all_backward:
+                fixed_path += "-LD"
+            else:
+                fixed_path += "LD"
         elif path[index].x == path[index + 1].x and path[index].y - 1 == path[index + 1].y:
-            fixed_path += "-L"
+            if not all_backward:
+                fixed_path += "-L"
+            else:
+                fixed_path += "L"
         elif path[index].x - 1 == path[index + 1].x and path[index].y - 1 == path[index + 1].y:
-            fixed_path += "-LU"
+            if not all_backward:
+                fixed_path += "-LU"
+            else:
+                fixed_path += "LU"
         elif path[index].x - 1 == path[index + 1].x and path[index].y == path[index + 1].y:
-            fixed_path += "-U"
+            if not all_backward:
+                fixed_path += "-U"
+            else:
+                fixed_path += "U"
         elif path[index].x - 1 == path[index + 1].x and path[index].y + 1 == path[index + 1].y:
-            fixed_path += "-RU"
+            if not all_backward:
+                fixed_path += "-RU"
+            else:
+                fixed_path += "RU"
         elif path[index].x == path[index + 1].x and path[index].y + 1 == path[index + 1].y:
-            fixed_path += "-R"
+            if not all_backward:
+                fixed_path += "-R"
+            else:
+                fixed_path += "R"
+        if all_backward and index != len(path) - 2:
+            fixed_path += "-"
     return fixed_path
 
